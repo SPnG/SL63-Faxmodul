b5b8f46c67d720a8aaa53b05f47812b21d3258dc
FBW <fw@speedpoint.de>
2013-08-09 13:17:45 +0200

    Skript durch erweiterte Version ersetzt

--- 002_Linux-Scripts/PUNKT_DisplayAusgabe.sh
+++ 002_Linux-Scripts/PUNKT_DisplayAusgabe.sh
@@ -11,14 +11,34 @@
 #
 ##############################################################################
 
+# alte NX-Cacheordner entfernen? (0 oder 1)
+rmnxcache="0"
+
+
 # los geht's:
 
-if [ -d .nx/C* ];then
-   Tkennung=`ls -d .nx/C* | awk -F"-" '{print $3}'`
+# =====================================================
+# alte NX-Cacheordner entfernen
+if [ $rmnxcache = "1" ]; then
+   if [ "`find .nx/ -name C-* -type d`" ];then
+      neu="`ls -tc .nx | grep '^C-' | sed -n '1p'`"
+      for i in `ls .nx | grep '^C-'`; do
+         if [ $i != $neu ]; then
+            rm -f $i
+         fi
+      done
+   fi
+fi
+# =====================================================
+
+
+if [ "`find .nx/ -name C-* -type d`" ];then
+   Tkennung="`ls -tc .nx | grep '^C-' | sed -n '1p' | awk -F"-" '{print $3}'`"
    Display=:$Tkennung.0
+
    echo $Display  >$HOME/DisplayAusgabe
 else
-   # Kein NX Arbeitsplatz
+   # Xdialog --infobox "Verzeichnis exitiert nicht" 0 0 2000
    echo ":0.0"  > $HOME/DisplayAusgabe
 fi
 
@@ -27,13 +47,13 @@ export DISPLAY=`cat $HOME/DisplayAusgabe`
 export XAUTHORITY=$HOME/.Xauthority
 
 Rueckgabe=`cat $HOME/DisplayAusgabe`
-if [ $DISPLAY = $Rueckgabe ]; then
+if [ $DISPLAY = $Rueckgabe ];then
    Xdialog --title "Initialisierung des Faxmoduls" \
            --infobox "$DISPLAY (ok)" 10 60 4000
 else
    Xdialog --title "Initialisierung des Faxmoduls" \
            --msgbox "ACHTUNG !!! DisplayAusgabe fehlerhaft gesetzt. \
-             Initialisierung des Faxmoduls fehlgeschlagen. $DISPLAY = $Rueckgabe" 0 0
+             Das Faxmodul wird nicht funktionieren $DISPLAY = $Rueckgabe" 0 0
 fi
 
 exit 0
